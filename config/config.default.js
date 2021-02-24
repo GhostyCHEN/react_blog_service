ce93046b24a1f6300db1563607d4d8301a71b88d
donggenmu <chenyinjiewj@gmail.com>
2021-02-24 18:11:20 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,62 @@
+/* eslint valid-jsdoc: "off" */
+
+"use strict";
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = (appInfo) => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1613894012402_223";
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: "121.4.120.180",
+      // port
+      port: "3306",
+      // username
+      user: "dgm",
+      // password
+      password: "ZXCasdqwe123!@#",
+      // database
+      database: "react_blog",
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+    domainWhiteList: ["*"],
+  };
+
+  config.cors = {
+    origin: "*",
+    allowMethods: "GET,HEAD.PUT,POST,DELETE,PATCH,OPTIONS",
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
